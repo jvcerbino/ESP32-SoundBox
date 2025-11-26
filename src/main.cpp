6428a706c7d2d63f5da2713828948c801fb13bbf
Joao Cerbino <joaovcerbino@gmail.com>
2025-11-26 19:49:26 -0300

    gui almost done

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,42 @@
 #include <lvgl.h>
 #include "BluetoothA2DPSink.h"
 
+/* MACROS */
 #define TFT_HOR_RES 320
 #define TFT_VER_RES 240
 #define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
 
+/* OBJETOS */
 static TFT_eSPI tft = TFT_eSPI();
 static lv_display_t *disp;
-void *draw_buf_1;
+void *draw_buf_1; 
 unsigned long lastTickMillis = 0;
-lv_obj_t *menu;
+lv_obj_t *menu; /* screen */
+static BluetoothA2DPSink a2dp_sink; 
 
-LV_IMAGE_DECLARE(next);
-LV_IMAGE_DECLARE(prev);
+lv_obj_t *artist_label;
+lv_obj_t *song_label;  
+lv_obj_t *vol_bar;
+lv_obj_t *vol_img;
+
+String lv_song = "Waiting...";
+String lv_artist = " ";
+String lv_duration = " ";
+int lv_vol = 0;
+bool meta_update = false;
+bool vol_update = false;
+
+/* IMAGENS */
 LV_IMAGE_DECLARE(pp);
 LV_IMAGE_DECLARE(vol);
-int i = 0;
+LV_IMAGE_DECLARE(no_vol);
 
-static BluetoothA2DPSink a2dp_sink;
-
-// Tem que mudar ainda
+/* INTERFACE GRÁFICA falta parse do ESP32-A2DP pra lvgl */
 void gui_draw()
 {
 
   menu = lv_obj_create(NULL);
-  lv_obj_remove_flag(menu, LV_OBJ_FLAG_CLICKABLE); /// Flags
+  lv_obj_remove_flag(menu, LV_OBJ_FLAG_CLICKABLE); 
   lv_obj_set_style_radius(menu, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_set_style_bg_color(menu, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_set_style_bg_opa(menu, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
@@ -42,34 +54,30 @@ void gui_draw()
   lv_obj_set_align(music_time, LV_ALIGN_BOTTOM_MID);
   lv_obj_set_style_bg_color(music_time, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_set_style_bg_opa(music_time, 30, LV_PART_MAIN | LV_STATE_DEFAULT);
-
   lv_obj_set_style_bg_color(music_time, lv_color_hex(0xFFFFFF), LV_PART_INDICATOR | LV_STATE_DEFAULT);
   lv_obj_set_style_bg_opa(music_time, 255, LV_PART_INDICATOR | LV_STATE_DEFAULT);
 
-  // change--
-  if (lv_obj_get_style_pad_top(music_time, LV_PART_MAIN) > 0)
-    lv_obj_set_style_pad_right(music_time, lv_obj_get_style_pad_right(music_time, LV_PART_MAIN) + 1, LV_PART_MAIN);
-
-  lv_obj_t *artist_label = lv_label_create(menu);
-  lv_obj_set_width(artist_label, LV_SIZE_CONTENT);  /// 1
-  lv_obj_set_height(artist_label, LV_SIZE_CONTENT); /// 1
-  lv_obj_set_x(artist_label, 0);
-  lv_obj_set_y(artist_label, -10);
-  lv_obj_set_align(artist_label, LV_ALIGN_CENTER);
-  lv_label_set_text(artist_label, "SONG");
-  lv_obj_set_style_text_color(artist_label, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
-  lv_obj_set_style_text_opa(artist_label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
-  lv_obj_set_style_text_font(artist_label, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
-
-  lv_obj_t *song_label = lv_label_create(menu);
-  lv_obj_set_width(song_label, LV_SIZE_CONTENT);
-  lv_obj_set_height(song_label, LV_SIZE_CONTENT);
+  song_label = lv_label_create(menu);
+  lv_obj_set_width(song_label, LV_SIZE_CONTENT); 
+  lv_obj_set_height(song_label, LV_SIZE_CONTENT); 
   lv_obj_set_x(song_label, 0);
-  lv_obj_set_y(song_label, 20);
+  lv_obj_set_y(song_label, -10);
   lv_obj_set_align(song_label, LV_ALIGN_CENTER);
-  lv_label_set_text(song_label, "ARTIST");
-  lv_obj_set_style_text_color(song_label, lv_color_hex(0xCCC3C3), LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_label_set_text(song_label, lv_song.c_str());
+  lv_obj_set_style_text_color(song_label, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_set_style_text_opa(song_label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_text_font(song_label, &lv_font_montserrat_20, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+  artist_label = lv_label_create(menu);
+  lv_obj_set_width(artist_label, LV_SIZE_CONTENT);
+  lv_obj_set_height(artist_label, LV_SIZE_CONTENT);
+  lv_obj_set_x(artist_label, 0);
+  lv_obj_set_y(artist_label, 20);
+  lv_obj_set_align(artist_label, LV_ALIGN_CENTER);
+  lv_label_set_text(artist_label, lv_artist.c_str());
+  lv_obj_set_style_text_color(artist_label, lv_color_hex(0xCCC3C3), LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_text_opa(artist_label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+  lv_obj_set_style_text_font(artist_label, &lv_font_montserrat_14, LV_PART_MAIN | LV_STATE_DEFAULT);
 
   lv_obj_t *pp_img = lv_image_create(menu);
   lv_image_set_src(pp_img, &pp);
@@ -79,23 +87,7 @@ void gui_draw()
   lv_obj_set_y(pp_img, -9);
   lv_obj_set_align(pp_img, LV_ALIGN_BOTTOM_MID);
 
-  lv_obj_t *next_img = lv_image_create(menu);
-  lv_image_set_src(next_img, &next);
-  lv_obj_set_width(next_img, LV_SIZE_CONTENT);
-  lv_obj_set_height(next_img, LV_SIZE_CONTENT);
-  lv_obj_set_x(next_img, 140);
-  lv_obj_set_y(next_img, -10);
-  lv_obj_set_align(next_img, LV_ALIGN_CENTER);
-
-  lv_obj_t *prev_img = lv_image_create(menu);
-  lv_image_set_src(prev_img, &prev);
-  lv_obj_set_width(prev_img, LV_SIZE_CONTENT);
-  lv_obj_set_height(prev_img, LV_SIZE_CONTENT);
-  lv_obj_set_x(prev_img, -136);
-  lv_obj_set_y(prev_img, -10);
-  lv_obj_set_align(prev_img, LV_ALIGN_CENTER);
-
-  lv_obj_t *vol_bar = lv_bar_create(menu);
+  vol_bar = lv_bar_create(menu);
   lv_bar_set_value(vol_bar, 25, LV_ANIM_OFF);
   lv_bar_set_start_value(vol_bar, 0, LV_ANIM_OFF);
   lv_obj_set_width(vol_bar, 150);
@@ -107,10 +99,7 @@ void gui_draw()
   lv_obj_set_style_bg_opa(vol_bar, 30, LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_set_style_bg_color(vol_bar, lv_color_hex(0xFFFFFF), LV_PART_INDICATOR | LV_STATE_DEFAULT);
   lv_obj_set_style_bg_opa(vol_bar, 255, LV_PART_INDICATOR | LV_STATE_DEFAULT);
-
-  // change--
-  if (lv_obj_get_style_pad_top(vol_bar, LV_PART_MAIN) > 0)
-    lv_obj_set_style_pad_right(vol_bar, lv_obj_get_style_pad_right(vol_bar, LV_PART_MAIN) + 1, LV_PART_MAIN);
+  lv_bar_set_range(vol_bar, 0, 127);
 
   lv_obj_t *timeleft_label = lv_label_create(menu);
   lv_obj_set_width(timeleft_label, LV_SIZE_CONTENT);
@@ -123,7 +112,7 @@ void gui_draw()
   lv_obj_set_style_text_opa(timeleft_label, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
   lv_obj_set_style_text_font(timeleft_label, &lv_font_montserrat_10, LV_PART_MAIN | LV_STATE_DEFAULT);
 
-  lv_obj_t *vol_img = lv_image_create(menu);
+  vol_img = lv_image_create(menu);
   lv_image_set_src(vol_img, &vol);
   lv_obj_set_width(vol_img, LV_SIZE_CONTENT);
   lv_obj_set_height(vol_img, LV_SIZE_CONTENT);
@@ -132,47 +121,79 @@ void gui_draw()
   lv_obj_set_align(vol_img, LV_ALIGN_CENTER);
 }
 
-// não ta saindo som direito pros DACs, TODO
-// static const i2s_config_t i2s_config = {
-//.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN), // ta bugado ainda!
-//.sample_rate = 44100,
-//.bits_per_sample = (i2s_bits_per_sample_t)16,
-//.channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-//.communication_format = I2S_COMM_FORMAT_I2S_MSB,
-//.intr_alloc_flags = 0,
-//.dma_buf_count = 8,
-//.dma_buf_len = 64,
-//.use_apll = false,
-//.tx_desc_auto_clear = true};
+/* Callback de atualização */
+
+void avrc_metadata_callback(uint8_t id, const uint8_t *text) {
+  String valor = String((char*)text);
+  
+  switch (id) {
+    case ESP_AVRC_MD_ATTR_TITLE:
+      lv_song = valor;
+      break;
+    case ESP_AVRC_MD_ATTR_ARTIST:
+      lv_artist = valor;
+      break;
+    case ESP_AVRC_MD_ATTR_PLAYING_TIME:
+      lv_duration = valor; 
+      break;
+  }
+  meta_update = true;
+}
+
+void volume_change_callback(int volume){
+  lv_vol = volume;
+  vol_update = true;
+}
+
 
 void setup()
 {
-
   tft.init();
   lv_init();
   draw_buf_1 = heap_caps_malloc(DRAW_BUF_SIZE, MALLOC_CAP_DMA | MALLOC_CAP_INTERNAL);
   disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf_1, DRAW_BUF_SIZE);
+
   static const i2s_config_t i2s_config = {
       .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
-      .sample_rate = 44100,                         // corrected by info from bluetooth
-      .bits_per_sample = (i2s_bits_per_sample_t)16, /* the DAC module will only take the 8bits from MSB */
+      .sample_rate = 44100,                        
+      .bits_per_sample = (i2s_bits_per_sample_t)16, 
       .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
       .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_STAND_MSB,
-      .intr_alloc_flags = 0, // default interrupt priority
+      .intr_alloc_flags = 0, 
       .dma_buf_count = 8,
       .dma_buf_len = 64,
       .use_apll = false};
+
   a2dp_sink.set_i2s_config(i2s_config);
   a2dp_sink.start("ESP32-SoundBox");
+  a2dp_sink.set_avrc_metadata_callback(avrc_metadata_callback);
+  a2dp_sink.set_on_volumechange(volume_change_callback);
   gui_draw();
 }
 
 void loop()
 {
+
+  if(meta_update){
+    lv_label_set_text(song_label, lv_song.c_str());
+    lv_label_set_text(artist_label, lv_artist.c_str());
+    meta_update = false;
+  }
+
+  if(vol_update){
+    if(lv_vol == 0){
+      lv_image_set_src(vol_img, &no_vol);
+    }
+    else{
+      lv_image_set_src(vol_img, &vol);
+    }
+    lv_bar_set_value(vol_bar, lv_vol, LV_ANIM_ON);
+    vol_update = false;
+  }
+
   unsigned int tickPeriod = millis() - lastTickMillis;
   lv_tick_inc(tickPeriod);
   lastTickMillis = millis();
-
   lv_task_handler();
   delay(10);
 }
